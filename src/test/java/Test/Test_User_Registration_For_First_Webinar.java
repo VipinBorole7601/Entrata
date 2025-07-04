893af1a6fc3b481510a152a1b8ebd18fb4e0f527
vipin <vipinborole@gmail.com>
2025-07-04 13:52:22 +0530

    Sys out message is updated to avoid confustion shows test failed soometimes

--- src/test/java/Test/Test_User_Registration_For_First_Webinar.java
+++ src/test/java/Test/Test_User_Registration_For_First_Webinar.java
@@ -1,15 +1,16 @@
 package Test;
 
 import Base.Base;
+import POM.mainPage_Register_WebinarPOM;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.log4testng.Logger;
 
 public class Test_User_Registration_For_First_Webinar extends Base {
-
+    mainPage_Register_WebinarPOM mp;
     private static final Logger logger = Logger.getLogger(Test_Entrata_Landing_Page.class);
 
-    @Test(priority = 1 ,description = "verify CurruntUrl of the page")
+    @Test(priority = 3 ,description = "verify CurruntUrl of the page")
     public void verify_CurrntUrl()
     {
         logger.info("Navigated to login page and verify url");
@@ -18,11 +19,14 @@ public class Test_User_Registration_For_First_Webinar extends Base {
         Assert.assertEquals(currunt_url,"https://www.entrata.com/" , "URL is matched");
         System.out.println("Currunt URL is correct matched with expected");
 
+        mp = new mainPage_Register_WebinarPOM(driver);
+        mp.closePrivacyPolicyPopup();
+
 
 
     }
 
-    @Test(priority = 2 ,description = "verify title of the page")
+    @Test(priority = 4 ,description = "verify title of the page")
     public void verify_Title()
     {
         logger.info("Navigated to login page and verify title");
@@ -31,11 +35,36 @@ public class Test_User_Registration_For_First_Webinar extends Base {
         System.out.println("title is matched");
     }
 
-    @Test(priority = 3,description = "Hover on Resources and click on Webinars")
+
+
+    @Test(priority = 5,description = "Hover on Resources and click on Webinars")
     public void verify_Registration_For_First_Webinar()
+    {     logger.info("Hover on Resources and click on Webinars");
+         mp.hover_On_Resource();
+         mp.click_On_Webinar();
+    }
+
+    @Test(priority = 6,description = "click on fist webinar as described in test case validate and fill the registration form and land back on main page")
+    public void register_For_Webinar()
     {
+        logger.info("validate and fill the registration form and land back on main page");
+        mp.click_And_click_to_Register_of_First_Webinar();
+
+        try {
+            wait(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
     }
+    @Test(priority = 7 ,description = "fill the registration form and comeback to main page")
+    public void fill_Registration_Form()
+    {
+        logger.info("form filling completed");
+          mp.fill_the_form();
+          System.out.println(" All Test for Register Webinar are Passed for Webinar Registration:  ");
+    }
+
 
 
 
